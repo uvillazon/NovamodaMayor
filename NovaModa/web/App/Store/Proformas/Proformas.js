d92022b300b6eba13c876141f76ed149a7b15573
= <=>
2015-10-07 23:49:09 -0400

    integrando con los otros servicios del sistema antigo

--- NovaModa/web/App/Store/Proformas/Proformas.js
+++ NovaModa/web/App/Store/Proformas/Proformas.js
@@ -4,7 +4,8 @@
 Ext.define('App.Store.Proformas.Proformas', {
 
     model: 'App.Model.Proformas.Proformas',
-    url: 'proformas/proformas',
+    //url: 'proformas/proformas',
+    url : 'http://sistema.novamoda-bo.com/mayor/php/Almacen.php?funcion=ListarAlmacen',
     sortProperty: 'id_proforma',
-    extend: 'App.Config.Abstract.Store'
+    extend: 'App.Config.Abstract.StoreJsonP'
 });
---
+++ NovaModa/web/App/View/Proformas/FormProforma.js
@@ -0,0 +1,20 @@
+﻿/* File Created: August 4, 2013 */
+
+Ext.Loader.setConfig({ enabled: true });
+Ext.Loader.setPath('App', 'App')
+Ext.require([
+    'App.*'
+]);
+
+Ext.application({
+    name: 'App',
+    appFolder: 'App',
+    controllers: ["App.controller.Proformas.Proformas"],
+    launch: function () {
+        Ext.tip.QuickTipManager.init();
+        var panel = Ext.create('App.View.Proformas.Principal', {
+            app : this
+            //            renderTo: 'contenido'
+        });
+    }
+});
